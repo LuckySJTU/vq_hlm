4439e9aee8c57a3cf86471fa1ac25f46390a73dc
LuckySJTU <942546416@qq.com>
2025-03-22 11:40:19 +0800

    fix spike loss

--- datasets/htoken.py
+++ datasets/htoken.py
@@ -23,14 +23,14 @@ class MyDataset(GeneratorBasedBuilder):
 
     def _generate_examples(self, filepath):
         raw_token = torch.load(os.path.join(self.base_path, filepath), weights_only=True)
-        for i in range(0, len(raw_token)-1, 1024):
-            input_ids = raw_token[i:i+1024]
-            label = raw_token[i+1:i+1025]
+        for i in range(0, len(raw_token)-1, 64):
+            input_ids = raw_token[i:i+64]
+            label = raw_token[i+1:i+65]
             if len(input_ids) > len(label):
                 input_ids = input_ids[:len(label)]
-            if len(input_ids) < 1024:
-                input_ids = torch.cat((input_ids, torch.zeros((1024-input_ids.shape[0], input_ids.shape[1]), dtype=input_ids.dtype)))
-                label = torch.cat((label, torch.zeros((1024-label.shape[0], label.shape[1]), dtype=label.dtype)*(-100)))
-                # input_ids = torch.cat((input_ids, raw_token[:1024-input_ids.shape[0]]))
-                # label = torch.cat((label, raw_token[1:1025-label.shape[0]]))
+            if len(input_ids) < 64:
+                input_ids = torch.cat((input_ids, torch.zeros((64-input_ids.shape[0], input_ids.shape[1]), dtype=input_ids.dtype)))
+                label = torch.cat((label, torch.zeros((64-label.shape[0], label.shape[1]), dtype=label.dtype)*(-100)))
+                # input_ids = torch.cat((input_ids, raw_token[:64-input_ids.shape[0]]))
+                # label = torch.cat((label, raw_token[1:65-label.shape[0]]))
             yield i, {"input_ids": input_ids, "label": label}
